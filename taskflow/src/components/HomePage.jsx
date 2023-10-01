66be0b88d026b162845be5bd6412cd2968375cc8
Shivam00002 <dubey02shiv@gmail.com>
2023-10-01 23:48:38 +0530

    added

--- taskflow/src/components/HomePage.jsx
+++ taskflow/src/components/HomePage.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export const HomePage = () => {
@@ -11,9 +12,15 @@ export const HomePage = () => {
             <div className="w-3 h-[23px] border rounded-md bg-violet-300"></div>
           </div>
           <h1 className="font-bold text-[25px]">TaskFlow</h1>
+          <Image src="https://cdn-icons-gif.flaticon.com/6172/6172541.gif"  alt="icon" width={37} height={37}/>
         </div>
         <div className="w-full h-[226px] mt-8 border">
      <p className="text-center text-[14px] tracking-[3px] text-gray-500 ">All BOARDS</p>
+
+
+     <div className="flex">
+
+     </div>
         </div>
       </div>
       <div className="h-[80px] w-full border"></div>

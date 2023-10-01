0fd85ef447aa6b80395d01dbef641014d4255151
Shivam00002 <dubey02shiv@gmail.com>
2023-10-01 23:28:41 +0530

    added react-icons

--- taskflow/src/components/HomePage.jsx
+++ taskflow/src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const HomePage = () => {
   return (
-    <div className=" max-w-[1440px] max-h-[1024px] mx-auto border border-red-600">
+    <div className=" max-w-[1440px] max-h-[1024px] mx-auto border border-red-600 flex">
       <div className="w-[300px] h-screen border">
         <div className="flex items-center gap-2 mx-auto w-fit py-1">
           <div className="flex items-center gap-1">
@@ -12,7 +12,11 @@ export const HomePage = () => {
           </div>
           <h1 className="font-bold text-[25px]">TaskFlow</h1>
         </div>
+        <div className="w-full h-[226px] mt-8 border">
+     <p className="text-center text-[14px] tracking-[3px] text-gray-500 ">All BOARDS</p>
+        </div>
       </div>
+      <div className="h-[80px] w-full border"></div>
     </div>
   );
 };

32c204ad09e65e76f005dbd9469a11cba1bbcae2
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-06-19 11:19:58 -0700

    Update HomePage.jsx

--- taskflow/src/components/HomePage.jsx
+++ taskflow/src/components/HomePage.jsx
@@ -7,6 +7,7 @@ export const HomePage = () => {
     <div className=" max-w-[1440px] max-h-[1024px] mx-auto border border-red-600 flex">
       <div className="w-[300px] h-screen border">
 
+
         <div className="flex items-center gap-2 mx-auto w-fit py-1">
           <div className="flex items-center gap-1">
             <div className="w-3 h-[23px] border rounded-md  bg-violet-200 "></div>
